2544231f671e6b558b81c015599f03ebd6d8ed0a
SampleUser <sampleuser@gmail.com>
2015-08-19 12:11:50 +0800

    Change hello world to hi

--- Sample/Sample/Form1.cs
+++ Sample/Sample/Form1.cs
@@ -16,5 +16,10 @@ namespace Sample
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label1.Text = "Hi";
+        }
     }
 }
